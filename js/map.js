bb8eb7a919ad4a2223462d27aa10bb07fe84de98
Brad Copenhaver and Devin Ludwig <devin@>
2017-01-05 16:22:50 -0800

    add variable size circle markers to map

--- js/map.js
+++ js/map.js
@@ -15,9 +15,10 @@ Mapper.prototype.initMap = function() {
   countryNames = ['spain', 'australia', 'argentina'];
   countryCoordinates = [[40.4, -3.68], [-35.27, 149.13], [-34.58, -58.7]];
   countryPosition = [];
+  countryObjects = [];
 
   function findColdplay(artist){
-      console.log(artist.name);
+    console.log(artist.name);
     return artist.name === "Coldplay";
   }
 
@@ -25,22 +26,29 @@ Mapper.prototype.initMap = function() {
     center: {lat: 45.2987294, lng: 1.3879601},
     zoom: 2
   });
-  // countryObjects = [];
 
+  var ind = 0;
 
   for(i=0; i< countryNames.length; i++){
-    $.get('http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=' + countryNames[i] + '&api_key=' + apiKey + '&format=json').done(function(response) {
-      position = response.topartists.artist.indexOf(response.topartists.artist.find(findColdplay));
-      countryPosition.push(position +1);
-    });
-  }
+    $.get('http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=' + countryNames[i] + '&api_key=' + apiKey + '&format=json').then(function(response) {
+      position = 1 + response.topartists.artist.indexOf(response.topartists.artist.find(findColdplay));
+      countryPosition.push(position);
+      console.log(position);
 
-  for (var i = 0; i < countryNames.length; i++) {
-    var coords = countryCoordinates[i];
-    var latLng = new google.maps.LatLng(coords[0],coords[1]);
-    var marker = new google.maps.Marker({
-      position: latLng,
-      map: map
+      newCountry = new Country(countryNames[ind], countryCoordinates[ind], position);
+      var coords = countryCoordinates[ind];
+      var latLng = new google.maps.LatLng(coords[0],coords[1]);
+      var cityCircle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: map,
+        center: latLng,
+        radius: 1000000 * Math.pow(position, -1)
+      });
+      ind++;
     });
   }
 };
